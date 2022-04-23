a8b8669c08df3dcd52e54c26ff667cca44e24d51
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2022-04-22 22:15:58 -0300

    Add files via upload

---
+++ Projeto2/script.js
@@ -0,0 +1,53 @@
+function verificar(){
+    var data = new Date()
+    var ano =  data.getFullYear()
+    var fAno = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+
+    if(fAno.value.length == 0 || fAno.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fAno.value)
+        var genero = ''
+
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+
+        if(fsex[0].checked){
+            genero = 'Homem'
+            if(idade >= 0 && idade < 10){
+                //crianca
+                img.setAttribute('src','foto-bebe-m.jpg')
+            }else if(idade < 21){
+                //jovem
+                img.setAttribute('src','foto-jovem-m.jpg')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src','foto-adulto-m.jpg')
+            }else{
+                //idoso
+                img.setAttribute('src','foto-idoso-m.jpg')
+            }
+        }else if(fsex[1].checked){
+            genero = 'Mulher'
+            if(idade >= 0 && idade < 10){
+                //crianca
+                img.setAttribute('src','foto-bebe-f.jpg')
+            }else if(idade < 21){
+                //joven
+                img.setAttribute('src','foto-jovem-f.jpg')
+            }else if(idade < 50){
+                //adulto
+                img.setAttribute('src','foto-adulto-f.jpg')
+            }else{
+                //idoso
+                img.setAttribute('src','foto-idoso-f.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML =  `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+
+}
